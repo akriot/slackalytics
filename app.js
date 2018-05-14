fe6f160b4a395b5e732d1b4ac743eb999cf0ee7b
Alok Ranjan <ak.ranjan@live.com>
2018-05-14 15:13:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ app.post('/collect', function(req, res){
 	//Structure Data
 	var data = {
 		v: 		1,
-		tid: 	env_var.ga_key,
+		tid: 	env_var.UA-114288944-1,
 		cid: 	user.id,
 		ds:  	"slack", //data source
 		cs: 	"slack", // campaign source
